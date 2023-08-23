d90d0ea919c6aa9a9b41d965087d7bbb1c163060
Lukas5937 <lukas-reich@t-online.de>
2023-08-23 08:13:36 +0200

    Test playRound function successfully

--- game.js
+++ game.js
@@ -12,8 +12,16 @@ function playRound(playerSelection, computerSelection) {
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
     else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
-    playerSelection === "Paper" && computerSelection === "Rock" ||
-    playerSelection === "Scissors" && computerSelection === "Paper") {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+            playerSelection === "Paper" && computerSelection === "Rock" ||
+            playerSelection === "Scissors" && computerSelection === "Paper") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `It's a draw! ${playerSelection} and ${computerSelection} are the same.`
+    }
+
 }
-}
+
+const computerSelection = getComputerChoice()
+
+console.log(playRound("Paper", computerSelection))
